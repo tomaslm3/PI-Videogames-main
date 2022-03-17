eff53476d7da96b72e4f26aae069c1ee157e021f
Tom <tomaslm_3@hotmail.co>
2022-03-17 18:57:07 -0300

    Work in home component

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllVideogames } from "../../redux/actions";
 
 function Home() {
+    const dispatch = useDispatch();
+    const allVideogames = useSelector(state => state.allVideogames)
+    
+    useEffect(() => {
+        dispatch(getAllVideogames())
+    },[])
+
     return(
-        <div>Hola desde Home</div>
+        <div>{allVideogames}</div>
+        
     )
 }
 
