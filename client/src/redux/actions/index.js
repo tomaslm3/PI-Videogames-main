d69299de6af30262b470704ca69b0fa1085e8e0c
Tom <tomaslm_3@hotmail.co>
2022-03-17 21:21:20 -0300

    Added base actions

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 export const actions = {
     GET_ALL_VIDEOGAMES: 'GET_ALL_VIDEOGAMES',
+    GET_VIDEOGAMES_BY_NAME: 'GET_VIDEOGAMES_BY_NAME',
+    GET_GENRES: 'GET_GENRES',
+    GET_VIDEOGAME_BY_ID: 'GET_VIDEOGAME_BY_ID',
+    POST_VIDEOGAME: 'POST_VIDEOGAME',
 };
 
-const { GET_ALL_VIDEOGAMES } = actions;
+const { GET_ALL_VIDEOGAMES, GET_VIDEOGAMES_BY_NAME, GET_GENRES, GET_VIDEOGAME_BY_ID, POST_VIDEOGAME } = actions;
 
 export function getAllVideogames() {
     return async function(dispatch) {
@@ -18,4 +22,61 @@ export function getAllVideogames() {
             console.log('Error action getAllVideogames ' + error)
         }
     }
-}
+};
+
+export function getVideogamesByName(payload) {
+    return async function (dispatch) {
+        try {
+            var json = await axios.get(`http://localhost:3001/videogames?name=${payload}`)
+            return dispatch({
+                type: GET_VIDEOGAMES_BY_NAME,
+                payload: json.data
+            })
+        } catch(error){
+            console.log('Error action getVideogamesByName ' + error)
+        }
+    }
+};
+
+export function getGenres() {
+    return async function (dispatch) {
+        try {
+            var json = await axios.get('http://localhost:3001/genres')
+            return dispatch({
+                type: GET_GENRES,
+                payload: json.data
+            })
+        } catch(error){
+            console.log('Error action getGenres ' + error)
+        }
+    }
+};
+
+export function getVideogameById(payload) {
+    return async function (dispatch) {
+        try {
+            var json = await axios.get(`http://localhost:3001/videogame/${payload}`)
+            return dispatch({
+                type: GET_VIDEOGAME_BY_ID,
+                payload: json.data
+            })
+        } catch(error){
+            console.log('Error action getVideogameById ' + error)
+        }
+    }
+};
+
+export function postVideogame(payload) {
+    return async function (dispatch) {
+        try {
+            var json = await axios.post('http://localhost:3001/videogame', payload)
+            return dispatch({
+                type: POST_VIDEOGAME,
+                payload: json.data
+            })
+        } catch (error) {
+            console.log('Error action newVideogame')
+        }
+    }
+};
+
--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,9 +1,11 @@
 import { actions } from "../actions";
-const {GET_ALL_VIDEOGAMES} = actions
+const {GET_ALL_VIDEOGAMES, GET_VIDEOGAMES_BY_NAME, GET_GENRES, GET_VIDEOGAME_BY_ID, POST_VIDEOGAME} = actions
 
 
 const initialState = {
-    allVideogames: []
+    allVideogames: [],
+    genres: [],
+    videogame: [],
 };
 
 function rootReducer(state = initialState, action) {
@@ -12,6 +14,25 @@ function rootReducer(state = initialState, action) {
             return {
                 ...state,
                 allVideogames: action.payload
+            };
+        case GET_VIDEOGAMES_BY_NAME:
+            return {
+                ...state,
+                allVideogames: action.payload
+            };
+        case GET_GENRES:
+            return {
+                ...state,
+                genres: action.payload
+            };
+        case GET_VIDEOGAME_BY_ID:
+            return {
+                ...state,
+                videogame: action.payload
+            };
+        case POST_VIDEOGAME:
+            return {
+                ...state,
             };
 
         default:
